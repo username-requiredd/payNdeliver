500f9ee07c85102569eee4eb703c108171bc9f00
username-requiredd <pj337167@gmail.com>
2024-11-06 12:31:02 -1100

    Deploying to vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   images: {
     remotePatterns: [
