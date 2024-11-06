39fa172351639549a13ec45daf9b29e3c59c2b7f
username-requiredd <pj337167@gmail.com>
2024-11-06 12:41:04 -1100

    Deploying to vercel

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Loader2, EyeOff, Eye } from "lucide-react";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
